b04154f81aa5a96f8f633b8a65239496430abfd5
Sparsh Chadha <sparsh.chadha@otpless.com>
2024-01-10 21:38:36 +0530

    Initial Commit

---
+++ OtplessDemoApplication/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  OtplessDemoApplication
+//
+//  Created by Sparsh on 10/01/24.
+//
+
+import UIKit
+import OtplessSDK
+
+class ViewController: UIViewController, onResponseDelegate {
+    func onResponse(response: OtplessSDK.OtplessResponse?) {
+        if let responseData = response?.responseData?["data"] as? [String: Any],
+           let mobileData = responseData["mobile"] as? [String: Any],
+           let name = mobileData["name"] as? String,
+           let number = mobileData["number"] as? String {
+            nameLabel.text = "Name : \(name)"
+            numberlabel.text = "Number : \(number)"
+        } else {
+            print("Error accessing mobile name and number in responseData")
+        }
+
+    }
+    
+
+    private var nameLabel: UILabel!
+    private var numberlabel: UILabel!
+    private var startOtplessButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+        setupConstraints()
+    }
+
+    private func setupUI() {
+        // Create and configure nameLabel
+        nameLabel = UILabel()
+        nameLabel.text = "Name : "
+        view.addSubview(nameLabel)
+
+        // Create and configure numberlabel
+        numberlabel = UILabel()
+        numberlabel.text = "Number : "
+        view.addSubview(numberlabel)
+
+        // Create and configure startOtplessButton
+        startOtplessButton = UIButton()
+        startOtplessButton.setTitle("Start OTP-less", for: .normal)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(startOtplessButtonTapped))
+        startOtplessButton.addGestureRecognizer(tapGesture)
+        view.addSubview(startOtplessButton)
+        
+        
+    }
+    
+    @objc private func startOtplessButtonTapped() {
+            // Handle tap action for startOtplessButton
+        Otpless.sharedInstance.delegate = self
+        var initialParams = [String:Any]()
+        initialParams["cid"] = "ef0kpz5g"
+        initialParams["crossButtonHidden"] = "true"
+        var params =  [String:Any]()
+        params["method"] = "get"
+        params["params"] = initialParams
+        Otpless.sharedInstance.showOtplessLoginPageWithParams(vc: self, params: params)
+    }
+
+    private func setupConstraints() {
+        // Disable autoresizing mask translation into constraints
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberlabel.translatesAutoresizingMaskIntoConstraints = false
+        startOtplessButton.translatesAutoresizingMaskIntoConstraints = false
+
+        // Center nameLabel vertically in the middle of the screen
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50), // Adjust top spacing as needed
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+
+        // Center numberlabel vertically below nameLabel
+        NSLayoutConstraint.activate([
+            numberlabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20), // Adjust vertical spacing as needed
+            numberlabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+
+        // Center startOtplessButton vertically below numberlabel
+        NSLayoutConstraint.activate([
+            startOtplessButton.topAnchor.constraint(equalTo: numberlabel.bottomAnchor, constant: 20), // Adjust vertical spacing as needed
+            startOtplessButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'OtplessDemoApplication' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for OtplessDemoApplication
+  pod 'OtplessSDK', '2.0.3'
+
+end
